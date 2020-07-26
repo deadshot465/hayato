2184b1efddec6364c85c0c93359bd5b817e04c11
Kirito1007 <mmkh1027@gmail.com>
2020-07-27 03:49:32 +0800

    prototype

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+client.run('NzM3MDE3MjMxNTIyOTIyNTU2.Xx3OyQ.YondP6gak5j5G4jzTJx88IKzPRM')
