55380752345be3482f2badafae5629d938a4523e
Chehui Chou <deadshot465@users.noreply.github.com>
2020-07-27 18:52:24 +0900

    Change to command extension.

--- main.py
+++ main.py
@@ -1,98 +1,116 @@
 import discord
 import random
 from datetime import datetime
+from discord.ext import commands
 
 
-class MyClient(discord.Client):
-    def __init__(self, **options):
-        super().__init__(**options)
-        # Add a series of games Hayato is going to play.
-        self.trains = ['Shinkansen E5', 'Shinkansen N700', 'Shinkansen L0', 'JR East KiHa 100', 'Shinkansen H5', 'Shinkansen E6', 'Shinkansen E7']
-        self.last_updated = datetime.now()
-        self.pings = ['pong', 'pang', 'pung']
+description = 'A Discord bot that can do some fun stuffs.'
+bot = commands.Bot(command_prefix='h!', description=description)
+trains = ['Shinkansen E5', 'Shinkansen N700', 'Shinkansen L0', 'JR East KiHa 100', 'Shinkansen H5',
+                       'Shinkansen E6', 'Shinkansen E7']
+last_updated = datetime.now()
+pings = ['pong', 'pang', 'pung']
 
-    # This function will switch Hayato's presence.
-    async def switch_presence(self):
-        game = discord.Game(random.choice(self.trains))
-        await client.change_presence(status=discord.Status.online, activity=game)
 
-    async def on_ready(self):
-        print('Logged on as', self.user)
-        await self.switch_presence()
+# Count vowels
+def count_vowels(string: str):
+    count = 0
+    for letter in string:
+        if letter in 'AEIOUaeiou':
+            count += 1
+    return count
 
-    async def on_message(self, message):
-        # Don't respond to ourselves
-        if message.author == self.user:
-            return
 
-        if message.content == 'h!ping':
-            await message.channel.send(random.choice(self.pings))
+def cvt_c_f(celsius: float) -> float:
+    '''(float) -> float
+    Return the temperature in Fahrenheit.
+    '''
+    fahrenheit = celsius * 9 / 5 + 32
+    return fahrenheit
 
-        if message.content.startswith('h!vowels'):
-            user_input = message.content[9:]
-            answer = self.count_vowels(user_input)
-            await message.channel.send('There are ' + str(answer) + ' vowels in the input!')
-        
-        if message.content.startswith('h!dashsep'):
-            user_input = message.content[10:]
-            answer = self.dash_separator(user_input)
-            await message.channel.send(str(answer))
-            
-        if message.content.startswith('h!cvt f c'):
-            user_input = message.content[10:]
-            answer = self.cvt_f_c(user_input)
-            await message.channel.send(str(user_input) + '\u2109' + ' is equal to ' + str(answer) + '\u2103.')
-            
-        if message.content.startswith('h!cvt c f'):
-            user_input = message.content[10:]
-            answer = self.cvt_c_f(user_input)
-            await message.channel.send(str(user_input) + '\u2103' + ' is equal to ' + str(answer) + '\u2109.')
-            
-        # Switch presence every hour
-        if (datetime.now() - self.last_updated).seconds > 3600:
-            await self.switch_presence()
 
-    # Count vowels
-    def count_vowels(self, string):
-        count = 0
-        for letter in string:
-            if letter in 'AEIOUaeiou':
-                count += 1
-        return count
-    
-    # Dash separator
-    def dash_separator(self, string):
-        '''(str) -> str
-        Returns a string the separates each letter of the input string with a dash.
-        
-        >>> dash_separator('hello')
-        h-e-l-l-o
-        >>> Kirito
-        K-i-r-i-t-o
-        '''
-        result = ''
-        i = 0
-        while i < len(string) - 1:
-            result = result + string[i] + '-'
-            i += 1
-        result += string[i]
-        return result
-    
-    # Convert �K to �J
-    def cvt_f_c(self, fahrenheit):
-        '''(float) -> float
-        Return the temperature in celsius.
-        '''
-        celsius = (int(fahrenheit) - 32) * 5 / 9
-        return celsius
-    
-     # Convert �J to �K
-    def cvt_c_f(self, celsius):
-        '''(float) -> float
-        Return the temperature in Fahrenheit.
-        '''
-        fahrenheit = int(celsius) * 9 / 5 + 32
-        return fahrenheit    
-    
-client = MyClient()
-client.run('NzM3MDE3MjMxNTIyOTIyNTU2.Xx3OyQ.YondP6gak5j5G4jzTJx88IKzPRM')
+def cvt_f_c(fahrenheit: float) -> float:
+    '''(float) -> float
+    Return the temperature in celsius.
+    '''
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+
+# Dash separator
+def dash_separator(string):
+    '''(str) -> str
+    Returns a string the separates each letter of the input string with a dash.
+
+    >>> dash_separator('hello')
+    h-e-l-l-o
+    >>> Kirito
+    K-i-r-i-t-o
+    '''
+    result = ''
+    i = 0
+    while i < len(string) - 1:
+        result = result + string[i] + '-'
+        i += 1
+    result += string[i]
+    return result
+
+
+# Switch presence every hour
+async def set_presence():
+    game = discord.Game(random.choice(trains))
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author == bot.user:
+        return
+    else:
+        await bot.process_commands(message)
+    global last_updated
+    if (datetime.now() - last_updated).seconds > 3600:
+        await set_presence()
+        last_updated = datetime.now()
+
+
+@bot.event
+async def on_ready():
+    print('Logged on as', bot.user)
+    await set_presence()
+
+
+@bot.command()
+async def ping(ctx: discord.ext.commands.Context):
+    await ctx.send(random.choice(pings))
+
+
+@bot.command()
+async def vowels(ctx: discord.ext.commands.Context, string: str):
+    if len(string) <= 0:
+        await ctx.send('No message found...')
+    answer = count_vowels(string)
+    await ctx.send('There are ' + str(answer) + ' vowels in the input!')
+
+
+@bot.command()
+async def dashsep(ctx: discord.ext.commands.Context, string: str):
+    if len(string) <= 0:
+        await ctx.send('No message found...')
+    answer = dash_separator(string)
+    await ctx.send(answer)
+
+
+@bot.command()
+async def cvt(ctx: discord.ext.commands.Context, unit1: str, unit2: str, number: float):
+    if unit1 == 'f' and unit2 == 'c':
+        answer = cvt_f_c(number)
+        await ctx.send(str(number) + '\u2109' + ' is equal to ' + str(answer) + '\u2103.')
+    elif unit1 == 'c' and unit2 == 'f':
+        answer = cvt_c_f(number)
+        await ctx.send(str(number) + '\u2103' + ' is equal to ' + str(answer) + '\u2109.')
+    else:
+        await ctx.send('The inputted parameters are incorrect!')
+
+
+bot.run('NzM3MDE3MjMxNTIyOTIyNTU2.Xx3OyQ.YondP6gak5j5G4jzTJx88IKzPRM')
