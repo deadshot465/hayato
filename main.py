48ed87d011e27a6e5a8305c9f46718a5610ccc2b
Chehui Chou <deadshot465@users.noreply.github.com>
2020-07-28 15:06:25 +0900

    Minor update.

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ class MyClient(discord.Client):
         # Don't respond to ourselves
         if message.author == self.user:
             return
+
+        if message.content == 'h!about':
+            avatar_url = str(self.user.avatar_url)
+            embed = discord.Embed(color=discord.Color.from_rgb(30, 99, 175),
+                                  description='Hayato is inspired by the anime Shinkalion. It is meant for practing making a Discord bot in Discord.py, but new features will be added from time to time.\n\nHayato version 1.0 was made and developed by:\n**Kirito#9286** and **Tetsuki Syu#1250**\nHayato Bot is licensed under GNU GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html').set_footer(
+                text='Hayato Bot: Release 1.0 | 2020-07-28').set_author(name='Hayasugi Hayato from Shinkalion',
+                                                                        icon_url=avatar_url).set_thumbnail(
+                url='https://pbs.twimg.com/profile_images/1245939758695510016/UOG9MGdU_400x400.png')
+            await message.channel.send(embed=embed)
         
         if message.content == 'h!help':
             await message.channel.send('Here is a list of commands for Hayato:\n`cvt` Convert units. Celsius/Fahrenheit and kg/lbs converter is available.\n`dashsep` Separate every letter of your input with a dash.\n`fascinated` Sends a certain number KouFascinated emote.\n`pick` Hayato will help you pick one choice randomly.\n`verifyemail` Check if the email is plausible.\n`vowels` Count the number of vowels in the input.')
