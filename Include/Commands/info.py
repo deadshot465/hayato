f720e013aeabf2ea4599ebf728797d56f9ab51ba
Chehui Chou <deadshot465@users.noreply.github.com>
2020-07-30 02:14:04 +0900

    Separate too multiple Python files.

---
+++ Include/Commands/info.py
@@ -0,0 +1,40 @@
+import discord
+import random
+from discord.ext import commands
+
+
+class InfoCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        super().__init__()
+        self.bot = bot
+        self.pings = ['pong', 'pang', 'peng', 'pung']
+
+    @commands.command()
+    async def about(self, ctx: commands.Context):
+        avatar_url = str(self.bot.user.avatar_url)
+        embed = discord.Embed(color=discord.Color.from_rgb(30, 99, 175),
+                              description='Hayato the best boi is inspired by the anime Shinkalion. It is meant for '
+                                          'practising making a Discord bot in Discord.py, but new features will be '
+                                          'added from time to time.\n\nHayato version 1.0 was made and developed '
+                                          'by:\n**Kirito#9286** and **Tetsuki Syu#1250**\nHayato Bot is licensed '
+                                          'under GNU GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html').set_footer(
+            text='Hayato Bot: Release 1.0 | 2020-07-28').set_author(name='Hayasugi Hayato from Shinkalion',
+                                                                    icon_url=avatar_url).set_thumbnail(
+            url='https://pbs.twimg.com/profile_images/1245939758695510016/UOG9MGdU_400x400.png')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def list(self, ctx: commands.Context):
+        await ctx.send('Here is a list of commands for Hayato:\n`cvt` Convert units. Celsius/Fahrenheit and kg/lbs '
+                       'converter is available.\n`dashsep` Separate every letter of your input with a '
+                       'dash.\n`fascinated` Sends a certain number KouFascinated emote.\n`pick` Hayato will help you '
+                       'pick one choice randomly.\n`verifyemail` Check if the email is plausible.\n`vowels` Count the '
+                       'number of vowels in the input.')
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        await ctx.send(random.choice(self.pings))
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(InfoCog(bot))
--- main.py
+++ main.py
@@ -1,68 +1,24 @@
 import discord
-import typing
 import random
 from datetime import datetime
 from discord.ext import commands
 
-
+# A simple description of our bot.
 DESCRIPTION = 'A Discord bot that can do some fun stuffs.'
-bot = commands.Bot(command_prefix='h!', description=DESCRIPTION)
+# Various trains for Hayato to play.
 TRAINS = ['Shinkansen E5', 'Shinkansen N700', 'Shinkansen L0', 'JR East KiHa 100', 'Shinkansen H5',
                        'Shinkansen E6', 'Shinkansen E7']
-last_updated = datetime.now()
-PINGS = ['pong', 'pang', 'pung']
+# Available domains.
 DOMAINS = {'com', 'net', 'hk', 'org', 'ca', 'info'}
+# Available cogs. Path is separated with dots, without file extensions.
+EXTENSIONS = ['Include.Commands.fun',
+              'Include.Commands.info',
+              'Include.Commands.utility']
 
-# Count vowels
-def count_vowels(string: str):
-    count = 0
-    for letter in string:
-        if letter in 'AEIOUaeiou':
-            count += 1
-    return count
-
-
-def cvt_c_f(celsius: float) -> float:
-    '''(float) -> float
-    Return the temperature in Fahrenheit.
-    '''
-    fahrenheit = celsius * 9 / 5 + 32
-    return fahrenheit
-
-
-def cvt_f_c(fahrenheit: float) -> float:
-    '''(float) -> float
-    Return the temperature in celsius.
-    '''
-    celsius = (fahrenheit - 32) * 5 / 9
-    return celsius
-
-
-def cvt_lb_kg(lbs: float) -> float:
-    return lbs / 2.2
-
-
-def cvt_kg_lb(kg: float) -> float:
-    return kg * 2.2
-
-
-# Dash separator
-def dash_separator(string):
-    '''(str) -> str
-    Returns a string the separates each letter of the input string with a dash.
-
-    >>> dash_separator('hello')
-    h-e-l-l-o
-    >>> Kirito
-    K-i-r-i-t-o
-    '''
-    result = ''
-    i = 0
-    while i < len(string) - 1:
-        result = result + string[i] + '-'
-        i += 1
-    result += string[i]
-    return result
+# Initialize our bot and set the prefix to 'h!', also set up the description.
+bot = commands.Bot(command_prefix='h!', description=DESCRIPTION)
+# Record the initial update time for switching presences every hour.
+last_updated = datetime.now()
 
 
 # Switch presence every hour
@@ -117,82 +73,6 @@ async def on_ready():
 
 
 @bot.command()
-async def ping(ctx: discord.ext.commands.Context):
-    await ctx.send(random.choice(PINGS))
-
-
-@bot.command()
-async def vowels(ctx: discord.ext.commands.Context, string: str):
-    if len(string) <= 0:
-        await ctx.send('No message found...')
-    answer = count_vowels(string)
-    await ctx.send('There are ' + str(answer) + ' vowels in the input!')
-
-
-@bot.command()
-async def dashsep(ctx: discord.ext.commands.Context, string: str):
-    if len(string) <= 0:
-        await ctx.send('No message found...')
-    answer = dash_separator(string)
-    await ctx.send(answer)
-
-
-@bot.command()
-async def cvt(ctx: discord.ext.commands.Context, unit1: str, unit2: str, number: float):
-    if unit1 == 'f' and unit2 == 'c':
-        answer = cvt_f_c(number)
-        await ctx.send(str(number) + '\u2109' + ' is equal to ' + str(answer) + '\u2103.')
-    elif unit1 == 'c' and unit2 == 'f':
-        answer = cvt_c_f(number)
-        await ctx.send(str(number) + '\u2103' + ' is equal to ' + str(answer) + '\u2109.')
-    elif unit1 == 'lbs' and unit2 == 'kg':
-        answer = cvt_lb_kg(number)
-        await ctx.send(str(number) + 'lbs is equal to ' + str(answer) + 'kg!')
-    elif unit1 == 'kg' and unit2 == 'lbs':
-        answer = cvt_kg_lb(number)
-        await ctx.send(str(number) + 'kg is equal to ' + str(answer) + 'lbs!')
-    else:
-        await ctx.send('The inputted parameters are incorrect!')
-
-
-@bot.command()
-async def pick(ctx: discord.ext.commands.Context, all_choices: str):
-    '''
-    Input several choices that split with "," and Hayato will help you choose one.
-    '''
-    error = False
-    # Separate the choices into a list
-    choices = all_choices.split(',')
-    # For each choice remove the spaces
-    for index in range(0, len(choices)):
-        choice = choices[index].strip(' ')
-        # If someone tries to fool Hayato, it is impossible
-        if choice == '':
-            error = True
-            break
-        # Replace the choice with the no-blank-space one
-        choices[index] = choice
-    # Pick a random choice from the list
-    hayato_choice = random.choice(choices)
-    # Return the choice
-    if error:
-        await ctx.send('Are you trying to fool me?')
-    else:
-        await ctx.send('I pick **' + hayato_choice + '** for you!')
-
-
-@bot.command()
-async def fascinated(ctx: discord.ext.commands.Context, count: typing.Optional[int] = 0):
-    result = ''
-    if str(count) == '' or count == 0:
-        result = 'https://cdn.discordapp.com/emojis/705279783340212265.gif'
-    else:
-        for i in range(0, count):
-            result += '<a:KouFascinated:705279783340212265> '
-    await ctx.send(result)
-
-
-@bot.command()
 async def verifyemail(ctx: discord.ext.commands.Context, email: str):
     '''
     (str) -> bool
@@ -243,17 +123,9 @@ async def verifyemail(ctx: discord.ext.commands.Context, email: str):
         await ctx.send('This email is plausible!')
 
 
-@bot.command()
-async def list(ctx: discord.ext.commands.Context):
-    await ctx.send('Here is a list of commands for Hayato:\n`cvt` Convert units. Celsius/Fahrenheit and kg/lbs converter is available.\n`dashsep` Separate every letter of your input with a dash.\n`fascinated` Sends a certain number KouFascinated emote.\n`pick` Hayato will help you pick one choice randomly.\n`verifyemail` Check if the email is plausible.\n`vowels` Count the number of vowels in the input.')
+if __name__ == '__main__':
+    for extension in EXTENSIONS:
+        bot.load_extension(extension)
+    print('Extensions successfully loaded.')
 
-
-@bot.command()
-async def about(ctx: discord.ext.commands.Context):
-    avatar_url = str(bot.user.avatar_url)
-    embed = discord.Embed(color=discord.Color.from_rgb(30, 99, 175), description='Hayato is inspired by the anime Shinkalion. It is meant for practing making a Discord bot in Discord.py, but new features will be added from time to time.\n\nHayato version 1.0 was made and developed by:\n**Kirito#9286** and **Tetsuki Syu#1250**\nHayato Bot is licensed under GNU GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html').set_footer(text='Hayato Bot: Release 1.0 | 2020-07-28').set_author(name='Hayasugi Hayato from Shinkalion', icon_url=avatar_url).set_thumbnail(url='https://pbs.twimg.com/profile_images/1245939758695510016/UOG9MGdU_400x400.png')
-    await ctx.send(embed=embed)
-
-
-
-bot.run('NzM3MDE3MjMxNTIyOTIyNTU2.Xx3OyQ.YondP6gak5j5G4jzTJx88IKzPRM')
+bot.run('NzM3MDE3MjMxNTIyOTIyNTU2.Xx3OyQ.YondP6gak5j5G4jzTJx88IKzPRM', bot=True, reconnect=True)
