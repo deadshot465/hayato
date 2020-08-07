6cf9bfdb0e8e490c7296a02ca8252c8b8a537e1b
Chehui Chou <deadshot465@users.noreply.github.com>
2020-08-07 10:12:18 +0900

    Update PowerShell script.

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
-Invoke-Expression "& `"./Scripts/activate`""
 Set-ExecutionPolicy Unrestricted -Force -Scope CurrentUser
+Invoke-Expression "& `"./Scripts/activate`""
 pip install -r requirements.txt
 Set-ExecutionPolicy Default -Force -Scope CurrentUser
