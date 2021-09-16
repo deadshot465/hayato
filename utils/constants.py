d9e2a2e44874c820b8bd34f5523c4369b799fcee
Chehui Chou <deadshot465@gmail.com>
2021-09-17 00:54:18 +0900

    Update version number and date.

--- utils/constants.py
+++ utils/constants.py
@@ -4,8 +4,8 @@ import hikari
 
 
 HAYATO_COLOR: typing.Final[hikari.Color] = hikari.Color(0x1e63af)
-VERSION: typing.Final[str] = '3.0.0'
-LAST_UPDATE_DATE: typing.Final[str] = '2021-09-10'
+VERSION: typing.Final[str] = '3.0.1'
+LAST_UPDATE_DATE: typing.Final[str] = '2021-09-17'
 PYTHON_LOGO: typing.Final[str] = \
     'https://cdn.discordapp.com/attachments/746200551007584337/810741576946941972/768px-Python-logo-notext' \
     '.svg.png'
